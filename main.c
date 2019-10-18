83a16f84d731f864566ad340f4bc111158a74d17
felfipe <victoramaral@usp.br>
2019-10-18 21:39:45 +0100

    Version 1.0

---
+++ main.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+main(int argc, char *argv[]){
+	
+	switch(argv[0][0]){
+		case '1':		// LEITURA DE DADOS
+
+
+		break;
+		case '2':		// RECUPERAÇÃO DE TODOS OS REGISTROS
+
+
+		break;
+		case '3':		// RECUPERAÇÃO POR BUSCA
+
+
+		break;
+		case '4':		// RECUPERAÇÃO DE REGISTROS POR RRN
+
+
+		break;
+		case '5':		// REMOÇÃO DE REGISTROS
+
+
+		break;
+		case '6':		// INSERÇÃO DE REGISTROS ADICIONAIS
+
+
+		break;
+		case '7':		// ATUALIZAÇÃO DE REGISTRO POR RRN
+
+		break;
+		case '8':		// COMPACTAÇÃO DO ARQUIVO
+
+
+		break;
+
+
+	}
+
+
+
+	return 0;
+}
