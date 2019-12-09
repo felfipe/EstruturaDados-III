2025e73569c49bcbdc7f2b8b7e6c2fd379c7ad2c
Andre Furlanetti <andrebpf@usp.br>
2019-12-09 20:56:58 -0300

    Final, parte gru

--- src/main.c
+++ src/main.c
@@ -1,75 +1,78 @@
 /*
-    TRABALHO DE ESTRUTURA DE DADOS III - PARTE 1
+    TRABALHO DE ESTRUTURA DE DADOS III - PARTE 3
 
     Grupo: André Baconcelo Prado Furlnaetti - 10748305
            Marcelo Magalhães Coelho         - 10716633
            Victor Felipe do Amaral          - 10696506
+    
+    Runcodes trabalho para correção: André Baconcelo Prado Furlnaetti - 10748305
 */
-
 
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<time.h>
 #include <limits.h>
+#include "funcoes_uteis.h"
 
 #define TAM_REGISTRO 85 // Define para o tamanho do registro
 #define TAM_ESTADO 2 // Tamanho fixo da string estado
 
 /*********************** STRUCTS *****************************/
 typedef struct{ // Estrutura do resgistro para ser manipulado
-    char estadoOrigem[3];
-    char estadoDestino[3];
-    int  distancia;
-    char cidadeOrigem[40];
-    char cidadeDestino[40];
-    char tempoViagem[10];
-} Route;
-typedef struct aresta Aresta;
-typedef struct vertice Vertice;
+    char estadoOrigem[3]; // String estado de origem
+    char estadoDestino[3]; // String estado de destino
+    int  distancia; // distância
+    char cidadeOrigem[40]; // String cidade origem
+    char cidadeDestino[40]; // String cidade destino
+    char tempoViagem[10]; // String tempo de viagem
+} Route; // Nome da strucit
 
-struct vertice{
-    char cidade[40];
-    char estado[3];
-    Aresta *aresta;
+typedef struct aresta Aresta; // Typedef da struct da aresta
+typedef struct vertice Vertice; // Typedef da struct do vértice
+
+struct vertice{ // Struct que representa os vértices do grafo
+    char cidade[40]; // String cidade
+    char estado[3]; // String estado
+    Aresta *aresta; // Cabeça da lista de arestas pertencentes ao vértice
 };
-struct aresta{
-    Vertice* destino;
-    char tempoViagem[10];
-    int distancia;
-    Aresta* prox;
+struct aresta{ // Struct que representa as arestas do grafo
+    Vertice* destino; // vértice do destino da aresta
+    char tempoViagem[10]; // String tempo de viagem
+    int distancia; // distância
+    Aresta* prox; // Ponteiro q aponta para a próxima arestas
 };
 // Estrutura de cabeçalho com 19 bytes
 typedef struct{ // Estrutura para manipulação do header
-    char status;
-    int numero_vertices;
-    int numero_arestas;
-    char data_ultima_compactacao[11];
-} Header;
+    char status; // Status do header
+    int numero_vertices; // número de vértices
+    int numero_arestas; // número de arestas
+    char data_ultima_compactacao[11]; // Data da última compactação
+} Header; // Nome da strutura
 
 
 /******************** FUNÇÕES AUXILIARES *************************/
 /*
-Insere ordenadamente um elemento do tipo aresta em uma lista
+Insere ordenadamente um elemento do tipo aresta em uma lista de forma ordenada
 Retorno:
 0 - ERRO
 1 - OK
 */
-int insere_lista_ordenada(Aresta** lista, Aresta elemento){
-    if(lista == NULL)
+int insere_lista_ordenada(Aresta** lista, Aresta elemento){ // Recebe a cabeça da lista de arestas e a aresta que será inserida
+    if(lista == NULL) // Se a cabeça da lista não foi alocada
         return 0;
     Aresta *no = (Aresta*) malloc(sizeof(Aresta)); // Aloca o nó
-    if(no == NULL)
+    if(no == NULL) // Se der erro
         return 0;
-    no->destino = elemento.destino;
+    no->destino = elemento.destino; // coloca os atributos do elemento no nó
     no->distancia = elemento.distancia;
     strcpy(no->tempoViagem,elemento.tempoViagem);
     if((*lista) == NULL){//lista vazia: insere início
         no->prox = NULL;
-        *lista = no;
-        return 1;
+        *lista = no; // cabeça da lista
+        return 1; // OK
     }
-    else{
+    else{ // procura onde deve ser inserido e reorganiza os ponteiros
         Aresta *ant, *atual = *lista;
         while(atual != NULL && (strcmp(atual->destino->cidade, no->destino->cidade) < 0)){
             ant = atual;
@@ -85,28 +88,29 @@ int insere_lista_ordenada(Aresta** lista, Aresta elemento){
         return 1;
     }
 }
+
 /*
 Libera a lista
 */
 void libera_lista(Aresta* li){
-        Aresta* no;
-        while(li != NULL){
-            no = li;
-            li = li->prox;
-            free(no);
-        }
+    Aresta* no; // para auxiliar ao percorrer a lista
+    while(li != NULL){
+        no = li;
+        li = li->prox;
+        free(no); // libera
+    }
 }
 
-/*Função que tenta abrir o arquivo informado. Caso o arquivo não exista ou seu status seja '0',
-printa a mensagem de erro e finaliza o programa
+/*
+    Função que tenta abrir o arquivo informado. Caso o arquivo não exista ou seu status seja '0',
+    printa a mensagem de erro e finaliza o programa
 */
-FILE *open_file_bin(char* file_name, char* mode){ 
-    FILE *file;
-    
-    file = fopen(file_name,mode);
-    if(file == NULL || getc(file) == '0'){
+FILE *open_file_bin(char* file_name, char* mode){  // Recebe o nome do arquivo e o modo que será aberto
+    FILE *file; // Ponteiro
+    file = fopen(file_name,mode); // abre o arquivo
+    if(file == NULL || getc(file) == '0'){ // verifica se realmente abriu e se o status é válido
         printf("Falha no carregamento do arquivo.");
-        exit(0);
+        exit(0); // encerra o programa
     }
     fseek(file,0,SEEK_CUR); // Volta no começo do arquivo
     return file;
@@ -131,8 +135,8 @@ void clear_route(Route *route){
 // Le uma string de tamanho variável do arquivo
 void read_variable_string(FILE *file_bin,char *string_var){
     int i = 0;
-    while(1){ // vai lendo até achar |
-        string_var[i] = fgetc(file_bin);
+    while(1){ // vai lendo até achar '|'
+        string_var[i] = fgetc(file_bin); // avança um byte
         if(string_var[i] == '|')
             break;
         i++;
@@ -140,15 +144,7 @@ void read_variable_string(FILE *file_bin,char *string_var){
     string_var[i] = '\0';
 }
 
-int dictionary_field(char *field_name){
-    char dictionary[6][15] = {"estadoOrigem","estadoDestino","distancia","cidadeOrigem","cidadeDestino","tempoViagem"};
-    for(int i = 0; i <6;i++){
-        if(!strcmp(field_name,dictionary[i]))
-            return i+1;
-    }
-    return -1;
-}
-
+// Le um registro pelo respectivo rnn
 int read_bin_rnn(FILE* file, int rnn, Route *route){
     clear_route(route); // Limpa o struct
     fseek(file,19,SEEK_SET); // Pula o cabeçalho
@@ -179,6 +175,8 @@ void read_header(FILE *file, Header *head){
     fread(&(head->numero_arestas),sizeof(int),1,file);
     fread(head->data_ultima_compactacao,10*sizeof(char),1,file);
 }
+
+// Limpa a struct aresta
 void limpa_aresta(Aresta *aresta){
     aresta->destino = NULL;
     aresta->distancia = 0;
@@ -186,11 +184,12 @@ void limpa_aresta(Aresta *aresta){
     strcpy(aresta->tempoViagem,"");
 }
 
-void printa_grafo(Vertice *cidade, int tam){
+// Printa o gráfico gerado por uma lista de adjacencia
+void printa_grafo(Vertice *cidade, int tam){ // Recebe o vetor de vértices e o tamanho
     for(int i = 0; i < tam; i++){ // percorre todo vetor de vértices
         printf("%s %s ",cidade[i].cidade, cidade[i].estado);
         for(Aresta *no = cidade[i].aresta; no != NULL; no = no->prox){ // percorre toda lista de cada vértice
-            if(strcmp("",no->tempoViagem) == 0)
+            if(strcmp("",no->tempoViagem) == 0) // Se estiver vazio
                 printf("%s %s %d ", no->destino->cidade,no->destino->estado,no->distancia);
             else
                 printf("%s %s %d %s ", no->destino->cidade,no->destino->estado,no->distancia,no->tempoViagem);
@@ -199,9 +198,8 @@ void printa_grafo(Vertice *cidade, int tam){
     }
 }
 
-/*
-Insere ordenadamente no vetor de Vértices
-*/
+// Insere ordenadamente no vetor de Vértices
+// Recebe o vetor de vértices, o tamanho, a cidade e o estado
 int insere_vetor_ordenado(Vertice* cidades, int tam, char* cidade, char* estado){
     int j = 0;
     while(j < tam){
@@ -220,6 +218,7 @@ int insere_vetor_ordenado(Vertice* cidades, int tam, char* cidade, char* estado)
     return 1;
 }
 
+// Ordena o vetor de vértices pelo método quicksort
 void quickSort(Vertice *aux, int inicio, int fim){
     
     int i, j;
@@ -265,14 +264,14 @@ TEM COMO RETORNO O TAMANHO DO VETOR DE VÉRTICES
 int gera_lista(FILE* file, Vertice **cidade){
     int i = 0;
     int cont = 0;
-    Route route;
-    Header header;
-    clear_route(&route);
-    read_header(file, &header);
+    Route route; // struct que representa o registro no disco
+    Header header; // struct q representa o header salvo no disco
+    clear_route(&route); // Limpa o struct
+    read_header(file, &header); // le o header do arquivo
     *cidade = (Vertice*)calloc(header.numero_vertices,sizeof(Vertice)); // ALOCA O VETOR DE VÉRTICES
     Vertice* vetor = *cidade;
     while(read_bin_rnn(file,i,&route) != -1){ // Lê todos os registros para a inserção dos vértices ordenadamente
-        if(route.estadoOrigem[0] != '*'){
+        if(route.estadoOrigem[0] != '*'){ // Se não estiver apagado
             if(insere_vetor_ordenado(*cidade,cont,route.cidadeOrigem,route.estadoOrigem) == 1)
                 cont++;
             if(insere_vetor_ordenado(*cidade,cont,route.cidadeDestino,route.estadoDestino) == 1)
@@ -286,7 +285,7 @@ int gera_lista(FILE* file, Vertice **cidade){
             for(int j = 0; j < header.numero_vertices; j++){ // Encontra no vetor de vértices a cidade de origem do registro
                 if(strcmp(vetor[j].cidade, route.cidadeOrigem) == 0){
                     Aresta aresta;
-                    limpa_aresta(&aresta);
+                    limpa_aresta(&aresta); // Limpa a struct aresta
                     strcpy(aresta.tempoViagem,route.tempoViagem);
                     aresta.distancia = route.distancia;
                     aresta.prox = NULL;
@@ -328,27 +327,35 @@ void dijkstra(FILE* file){
 
 
 /**************** FUNÇÃO 11 ********************/
-void prim(FILE *file){
-    Vertice* lista;
-    int num_vertices = gera_lista(file,&lista); // Gera o grafo a partir do arquivo
-    Vertice conjunto_b[num_vertices]; // Conjunto de vértices analizados
-    Vertice aux; //
-    Aresta aresta;
-    Vertice vertice_add;
-    int tam_conjunto_b=0;
-    int min = INT_MAX;
-    int indice_vertice;
-    char campo[40];
-    char vertice_inicial[40];
+// Função que recebe um ponteiro de arquivo, utiliza a função descrita no item 9 para gerar o gráfo,
+// a partir deste, encontra a árvore geradora mínima conforme o algorítimo de Prim
 
-    scanf("%s", campo);
-    scan_quote_string(vertice_inicial);
+void prim(FILE *file){
+    Vertice* vet_vertices; // vetor de vértices, representa o conjunto de todos os vértices, chamado de N
+    int num_vertices = gera_lista(file,&vet_vertices); // Gera o grafo a partir do arquivo
+    Vertice conjunto_b[num_vertices]; // Conjunto de vértices analizados, chamado de B
+    Vertice aux; // auxilia pra percorrer as listas
+    Aresta aresta; // aresta que será incerida no vértice correspondente do conjunto B
+    Vertice vertice_add; // Vértice que possui o meno peso, será inserido no conjunto B
+    int tam_conjunto_b=0; // tamanho do conjunto B
+    int min = INT_MAX; // Peso das arestas (distâncias), INT_MAX esta representando o "infinito"
+    int indice_vertice; // Índice do vértice em q a aresta será inserida
+    char campo[40]; // Campo de entrada
+    char vertice_inicial[40]; // Nome da cidade do vértice inicial
+
+    scanf("%s", campo); // Recebe o campo
+    if(strcmp(campo,"cidadeOrigem") != 0){
+        printf("Falha na execução da funcionalidade.");
+        exit(0); // encerra o programa
+    }
+
+    scan_quote_string(vertice_inicial); // Recebe o nome da cidade do vértice inicial
 
     int i = 0;
     for(i=0; i<num_vertices; i++){ // Procura o vértice inical e o adiciona no conjunto B
-        if(strcasecmp(lista[i].cidade,vertice_inicial) == 0){
-            strcpy(conjunto_b[0].cidade,lista[i].cidade);
-            strcpy(conjunto_b[0].estado,lista[i].estado);
+        if(strcasecmp(vet_vertices[i].cidade,vertice_inicial) == 0){
+            strcpy(conjunto_b[0].cidade,vet_vertices[i].cidade);
+            strcpy(conjunto_b[0].estado,vet_vertices[i].estado);
             conjunto_b->aresta = NULL;
             break;
         }
@@ -359,43 +366,43 @@ void prim(FILE *file){
     }
     tam_conjunto_b++;
 
-    while(tam_conjunto_b != num_vertices){ // Enquanto B != N
+    while(tam_conjunto_b != num_vertices){ // Enquanto B != N, condição de parada
         min = INT_MAX; // min = "infinito"
-        for(int cont_vetices = 0; cont_vetices < num_vertices; cont_vetices++){ // Para todos os vértices do grafo
-            for(int cont_conj_b = 0; cont_conj_b < tam_conjunto_b; cont_conj_b++){
-                if(strcasecmp(lista[cont_vetices].cidade,conjunto_b[cont_conj_b].cidade) == 0){ // Se o vértice estiver no conjunto B
-                    aux = lista[cont_vetices];
-
-                    while (aux.aresta != NULL){
-                        int esta_conj_N_B = 1;
+        for(int cont_vetices = 0; cont_vetices < num_vertices; cont_vetices++){ // Para percorrer todos os vértices do grafo
+            for(int cont_conj_b = 0; cont_conj_b < tam_conjunto_b; cont_conj_b++){ // Percorre o vetor do conjunto B
+                if(strcasecmp(vet_vertices[cont_vetices].cidade,conjunto_b[cont_conj_b].cidade) == 0){ // Se o vértice estiver no conjunto B
+                    aux = vet_vertices[cont_vetices]; // Recebe o vértice encontrado
+                    while (aux.aresta != NULL){ // Se possuir arestas
+                        int esta_conj_N_B = 1; // Flag para verificar se o vértice está no conjunto N - B
                         for(int j=0; j<tam_conjunto_b;j++){ // Verifica se está no conjunto N - B
                             if(strcasecmp(aux.aresta->destino->cidade,conjunto_b[j].cidade) == 0) {
-                                esta_conj_N_B = 0;
+                                esta_conj_N_B = 0; // Se não estiver
                                 break;
                             }
                         }
                         if(esta_conj_N_B){ // Está no conjunto N - B
-                            if(min > aux.aresta->distancia){ // Se min > custo da aresta
-                                min = aux.aresta->distancia;
-                                vertice_add = *aux.aresta->destino;
+                            if(min > aux.aresta->distancia){ // Se min > peso da aresta
+                                min = aux.aresta->distancia; // Recebe o novo mínimo
+                                vertice_add = *aux.aresta->destino; // recebe o vértice
                                 vertice_add.aresta = NULL;
-                                indice_vertice = cont_conj_b;
-                                limpa_aresta(&aresta);
-                                aresta = *aux.aresta;
+                                indice_vertice = cont_conj_b; // o indíce no conjunto_b
+                                limpa_aresta(&aresta); // Limpa a aresta
+                                aresta = *aux.aresta; // recebe a resta
                                 aresta.prox = NULL;
                             }
                         }
-                        aux.aresta = aux.aresta->prox;
+                        aux.aresta = aux.aresta->prox; // vai para a próxima aresta
                     }
                 }
             }
         }
-        insere_lista_ordenada(&(conjunto_b[indice_vertice].aresta),aresta);
-        conjunto_b[tam_conjunto_b] = vertice_add;
-        tam_conjunto_b++;
+        // Após percorrer todos os vértices
+        insere_lista_ordenada(&(conjunto_b[indice_vertice].aresta),aresta); // insere a aresta no vértice do conjunto B
+        conjunto_b[tam_conjunto_b] = vertice_add; // Adiciona o novo vértice
+        tam_conjunto_b++; // mais um no tamanho do conjunto B
     }
-    quickSort(conjunto_b,0,tam_conjunto_b-1);
-    printa_grafo(conjunto_b,tam_conjunto_b);
+    quickSort(conjunto_b,0,tam_conjunto_b-1); // Ordena o conjunto (vetor) B
+    printa_grafo(conjunto_b,tam_conjunto_b); // Printa o gráfico (árvore geradora mínima) encontrado
 }
 
 
